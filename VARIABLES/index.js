58b7976759badfbaa645c041f096659026e395e5
Vila71 <lolivila71@gmail.com>
2023-11-21 18:38:29 +0100

    primer cambio

---
+++ VARIABLES/index.js
@@ -0,0 +1,51 @@
+/*Crear variables de diferentes tipos de datos: número, cadena de texto, booleano e imprimir su valor por
+la pantalla del navegador y la consola*/
+//numero
+let numero= 1;
+console.log(numero)
+document.write(numero)
+
+//cadena de texto
+let saludo = "hola chicas";
+console.log(saludo)
+document.write(saludo)
+
+//boleanos
+let red= false ;
+console.log(red)
+document.write(red)
+
+let white= true ;
+console.log(white)
+document.write(white)
+
+//la única que es integer sería la de tipo número ya que es entero
+
+/*realiza operaciones matemáticas básicas con las variables creadas anteriormente (suma, resta, multiplicación, división).*/
+
+let suma = 2+2
+suma 
+console.log(suma);
+document.write(suma);
+
+let resta = 2-2
+suma 
+console.log(resta);
+document.write(resta);
+
+let multiplicación = 2*2
+suma 
+console.log(multiplicación);
+document.write(multiplicación);
+
+let división = 2/2
+suma 
+console.log(división);
+document.write(división);
+
+/*De las variables declaradas anteriormente, identifica las que son de tipo texto y concatena dichas
+cadenas de texto*/
+
+let bienvenida= "hola "+"chicas "
+console.log(bienvenida);
+document.write(bienvenida);
